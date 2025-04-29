5a02cccf6eba068ad1458b9b7b9e3eafceb553fe
Nick <73343187+regular-nick@users.noreply.github.com>
2025-04-29 14:09:50 +0000

    made initial nodes

--- lisp-book/magic.lisp
+++ lisp-book/magic.lisp
@@ -1,1 +1,10 @@
-(defparameter 
+(defparameter *lr-desc* '(you are in the living-room.))
+(defparameter *attic-desc* '(you are in the attic.))
+(defparameter *garden-desc* '(you are in the garden.))
+
+(defparameter *nodes* 
+`(
+    (living-room ,*lr-desc*)
+    (attic ,*attic-desc*)
+    (garden ,*garden-desc*)
+))
