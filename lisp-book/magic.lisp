8a90360b952ae5261e62b630cfa1be59b4fa95bc
Nick <73343187+regular-nick@users.noreply.github.com>
2025-04-30 11:15:16 +0000

    expanded descriptions; added secret well node

--- lisp-book/magic.lisp
+++ lisp-book/magic.lisp
@@ -1,10 +1,12 @@
-(defparameter *lr-desc* '(you are in the living-room.))
-(defparameter *attic-desc* '(you are in the attic.))
-(defparameter *garden-desc* '(you are in the garden.))
+(defparameter *lr-desc* '(you are in the living-room. a wizard is snoring loudly on the couch))
+(defparameter *attic-desc* '(you are in the attic. there is a well in front of you.))
+(defparameter *well-desc* '(?????))
+(defparameter *garden-desc* '(you are in the garden. there is a giant welding torch in the corner.))
 
 (defparameter *nodes* 
 `(
     (living-room ,*lr-desc*)
     (attic ,*attic-desc*)
     (garden ,*garden-desc*)
+    (well ,*well-desc*)
 ))
